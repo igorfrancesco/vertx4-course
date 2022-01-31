9026ce93d7a389cafea6eb27c87a54eb882da8c2
Igor Francesco A Rodrigues <igorfrancesco2@gmail.com>
2022-01-30 21:56:30 -0300

    TestThreads and Events control

--- src/main/java/br/com/udemy/vertx_starter/verticles/MainVerticle.java
+++ src/main/java/br/com/udemy/vertx_starter/verticles/MainVerticle.java
@@ -1,4 +1,19 @@
 package br.com.udemy.vertx_starter.verticles;
 
-public class MainVerticle {
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Promise;
+import io.vertx.core.Vertx;
+
+public class MainVerticle extends AbstractVerticle {
+
+  public static void main(String[] args) {
+    final Vertx vertx = Vertx.vertx();
+    vertx.deployVerticle(new MainVerticle());
+  } // FIM DO MAIN
+
+  @Override
+  public void start(final Promise<Void> stVoidPromise) throws Exception{
+    System.out.println("Start " + getClass().getName());
+  }
+
 }
