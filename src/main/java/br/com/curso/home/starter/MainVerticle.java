bf811aa514061a46ea3f2c2d2c33510513b81476
Igor Francesco A Rodrigues <igorfrancesco2@gmail.com>
2022-02-21 07:47:03 -0300

    Dependency log4j

--- src/main/java/br/com/curso/home/starter/MainVerticle.java
+++ src/main/java/br/com/curso/home/starter/MainVerticle.java
@@ -2,8 +2,14 @@ package br.com.curso.home.starter;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Promise;
+import io.vertx.core.Vertx;
 
 public class MainVerticle extends AbstractVerticle {
+
+  public static void main(String[] args) {
+    final Vertx vertx = Vertx.vertx();
+    vertx.deployVerticle(new MainVerticle());
+  }
 
   @Override
   public void start(Promise<Void> startPromise) throws Exception {
